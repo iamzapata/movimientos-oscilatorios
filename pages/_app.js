50d72aa137c767b263416ebb0e1f6598bbffd960
Andres Zapata <me@azapata.io>
2020-06-06 15:30:19 -0500

    Arreglar problem con .flex

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,7 @@ import React from "react";
 import Head from "next/head";
 import Navbar from "components/Navbar";
 
-import "bulma";
-import "pages/styles.css";
-import "katex/dist/katex.min.css";
-import "tailwindcss/dist/tailwind.min.css";
-import "font-awesome/css/font-awesome.min.css";
-import "normalize.css";
+import "pages/styles.scss";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
