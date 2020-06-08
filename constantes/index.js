514790662058abc43d99d9a5f6bc809e40a503c4
Andres Zapata <me@azapata.io>
2020-06-07 19:04:49 -0500

    Agregar jsdoc al proyecto

--- constantes/index.js
+++ constantes/index.js
@@ -1,4 +1,16 @@
+/**
+ * Constante pi 3.1416...
+ * 
+ * @type {numero}
+ */
 const PI = Math.PI;
+
+/**
+ * Constante pi x 2 6.2813
+ * 
+ * @type {numero}
+ */
 const PI2 = Math.PI * 2;
+
 
 export { PI, PI2 };
